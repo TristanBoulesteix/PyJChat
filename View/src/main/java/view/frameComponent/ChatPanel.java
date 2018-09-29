a1ae0be0480a5692efb37e9ad6f63414c93df576
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-09-29 19:31:29 +0200

    Creating design of main panel

--- View/src/main/java/view/frameComponent/ChatPanel.java
+++ View/src/main/java/view/frameComponent/ChatPanel.java
@@ -1,32 +1,48 @@
 package view.frameComponent;
 
+import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatPanel extends JPanel {
-	private static final long serialVersionUID = 186327137878714376L;
+	private static final long serialVersionUID = 6339287783087067958L;
 
+	private JPanel textArea;
 	private JTextField keyZone;
-	private JTextArea textArea;
+	private JButton sendButton;
 
 	public ChatPanel() {
 		this.setLayout(new GridBagLayout());
 
-		this.textArea = new JTextArea();
+		this.textArea = new JPanel();
 		GridBagConstraints cTextArea = new GridBagConstraints();
 		cTextArea.gridx = 0;
 		cTextArea.gridy = 0;
+		cTextArea.weightx = 1.0;
+		cTextArea.weighty = 1.0;
 		cTextArea.fill = GridBagConstraints.BOTH;
 		this.add(this.textArea, cTextArea);
 
+		JPanel pKeyZone = new JPanel();
+		pKeyZone.setLayout(new BorderLayout());
+		GridBagConstraints cPKeyZone = new GridBagConstraints();
+		cPKeyZone.gridx = 0;
+		cPKeyZone.gridy = 1;
+		cPKeyZone.weightx = 1.0;
+		cPKeyZone.fill = GridBagConstraints.BOTH;
+		this.add(pKeyZone, cPKeyZone);
+
 		this.keyZone = new JTextField();
-		GridBagConstraints cKeyZone = new GridBagConstraints();
-		cKeyZone.gridx = 0;
-		cKeyZone.gridy = 1;
-		this.add(this.keyZone, cKeyZone);
+		this.keyZone.setEnabled(false);
+		pKeyZone.add(this.keyZone, BorderLayout.CENTER);
+
+		this.sendButton = new JButton("Envoyer");
+		this.sendButton.setEnabled(false);
+		pKeyZone.add(this.sendButton, BorderLayout.EAST);
 	}
+
 }
