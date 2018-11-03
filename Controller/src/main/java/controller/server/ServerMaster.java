1a03f40dca82dc812265d5b9c82ed5d45e8734da
TristanBoulesteix <tristan.boulesteix@gmail.com>
2018-11-03 18:53:41 +0100

    update ressources server.py

--- Controller/src/main/java/controller/server/ServerMaster.java
+++ Controller/src/main/java/controller/server/ServerMaster.java
@@ -1,15 +1,21 @@
 package controller.server;
 
+import java.io.File;
+
 import org.python.util.PythonInterpreter;
 
 public class ServerMaster {
-	private static final String SERVER_PATH = "/src/main/java/controller/server/Server.py";
+	private static final String SERVER_PATH = "Controller/main/java/controller/server/server.py";
 
 	private PythonInterpreter pythonInterpreter;
 
 	public ServerMaster() {
 		PythonInterpreter.initialize(System.getProperties(), System.getProperties(), new String[0]);
 		this.pythonInterpreter = new PythonInterpreter();
-		this.pythonInterpreter.execfile(SERVER_PATH);
+
+		File pyServer = new File(SERVER_PATH);
+		System.out.println(pyServer.getAbsolutePath());
+
+		this.pythonInterpreter.execfile(pyServer.getAbsolutePath());
 	}
 }
